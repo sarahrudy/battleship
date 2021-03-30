8a257679b645e23c1f20104c15bf1ce4e1bb7434
Mitch Standley <mitch_standley@yahoo.com>
2021-03-29 20:31:01 -0500

    Update hit method

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -14,9 +14,33 @@ describe Ship do
     expect(cruiser.name).to eq("Cruiser")
   end
 
-  it 'has a size' do
+  it 'has a length' do
     cruiser = Ship.new("Cruiser", 3)
 
     expect(cruiser.length).to eq(3)
   end
+
+  it 'has full health' do
+    cruiser = Ship.new("Cruiser", 3)
+
+    expect(cruiser.health).to eq(3)
+  end
+
+  it 'is sunk?' do
+    cruiser = Ship.new("Cruiser", 3)
+
+    expect(cruiser.sunk?).to eq(false)
+  end
+
+  it 'took a hit' do
+    cruiser = Ship.new("Cruiser", 3)
+
+    cruiser.hit
+    expect(cruiser.health).to eq(2)
+    cruiser.hit
+    expect(cruiser.health).to eq(1)
+    cruiser.hit
+    expect(cruiser.sunk?).to eq(true)
+  end
+
 end
