ec140e59642c0f5dc9651c6de15b82e8eb18d675
Sarah Rudy <sarahrudy@gmail.com>
2021-03-30 19:06:17 -0600

    Add contents to 'cell' files

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,4 +1,7 @@
 class Cell
-  
+  attr_reader :coordinate
 
+  def initialize(coordinate)
+    @coordinate  = coordinate
+  end
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,2 +1,19 @@
-require './ship'
-require './cell'
+require 'rspec'
+require './lib/ship'
+require './lib/cell'
+
+describe Cell do
+  it 'exists' do
+    cell = Cell.new("B4")
+
+    expect(cell).to be_instance_of(Cell)
+  end
+
+  it 'has a coordinate' do
+    cell = Cell.new("B4")
+
+    expect(cell.coordinate).to eq("B4")
+  end
+
+
+end
