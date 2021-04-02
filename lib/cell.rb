93d21b1b227d29f3f0172a714a088bc01f5a4a70
Sarah Rudy <sarahrudy@gmail.com>
2021-04-01 20:51:49 -0600

    Finish 'iteration 1' exit quit k git

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,12 +1,12 @@
 class Cell
   attr_reader :coordinate,
-              :ship,
-              :fired_upon
+  :ship,
+  :fired_upon
 
   def initialize(coordinate)
     @coordinate     = coordinate
     @ship           = ship
-                    # (name, length, health)
+    # (name, length, health)
     @fired_upon     = false
   end
 
@@ -26,28 +26,22 @@ class Cell
 
   def fired_upon? # checking status of attribute
     @fired_upon
-  # empty, miss, hit, sunk
-  # check to see if it changed
+    # empty, miss, hit, sunk
+    # check to see if it changed
   end
 
   def fire_upon
-    @ship.hit
-    @fired_upon = true
-  end
-
-  def render
-    if @ship == nil
-      return "."
-      if @fired_upon
-        return "M"
-      end
+    if @ship != nil
+      @ship.hit
     end
-   #if cell is empty, return "."
-    #if cell has been fired upon but has no ship, return "M"
-    #if cell has been fired upon and has a ship, return "H"
-    #if cell has been fired upon and has a ship that is now sunk, return "X"
-    #if cell has a ship, return "S"
-  end
-  # one more method!
+      @fired_upon = true
+    end
 
+  def render(reveal = false)
+    return "M" if @fired_upon == true && empty?
+    return "X" if @fired_upon == true && @ship.sunk?
+    return "S" if reveal == true && !empty?
+    return "H" if @fired_upon == true && !empty?
+    return "."
+  end
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -3,30 +3,27 @@ require './lib/ship'
 require './lib/cell'
 
 describe Cell do
+
   it 'exists' do
     cell = Cell.new("B4")
-    @cruiser = Ship.new("Cruiser", 3)
 
     expect(cell).to be_instance_of(Cell)
   end
 
   it 'has attributes' do
     cell = Cell.new("B4")
-    @cruiser = Ship.new("Cruiser", 3)
 
     expect(cell.coordinate).to eq("B4")
   end
 
   it 'has a ship' do
     cell = Cell.new("B4")
-    @cruiser = Ship.new("Cruiser", 3)
 
     expect(cell.ship).to eq(nil)
   end
 
   it 'has an empty cell' do
     cell = Cell.new("B4")
-    @cruiser = Ship.new("Cruiser", 3)
 
     expect(cell.empty?).to eq(true)
   end
@@ -43,6 +40,7 @@ describe Cell do
   it 'is fired upon' do
     cell = Cell.new("B4")
     cruiser = Ship.new("Cruiser", 3)
+
     cell.place_ship(cruiser)
 
     expect(cell.fired_upon?).to eq(false) # brand new cell, no action yet
@@ -51,11 +49,36 @@ describe Cell do
     expect(cell.fired_upon?).to eq(true)
   end
 
-  it 'knows if it has been fired upon' do
+  it 'renders correct symbols' do
     cell_1 = Cell.new("B4")
+    cell_2 = Cell.new("C3")
+    cruiser = Ship.new("Cruiser", 3)
 
     expect(cell_1.render).to eq(".")
+
     cell_1.fire_upon
+
     expect(cell_1.render).to eq("M")
+
+    cell_2.place_ship(cruiser)
+
+    expect(cell_2.render).to eq(".")
+
+    expect(cell_2.render(true)).to eq("S")
+
+    cell_2.fire_upon
+
+    expect(cell_2.render).to eq("H")
+
+    expect(cruiser.sunk?).to eq(false)
+
+    cruiser.hit
+    cruiser.hit
+
+    expect(cruiser.sunk?).to eq(true)
+
+    expect(cell_2.render).to eq("X")
   end
+
+
 end
