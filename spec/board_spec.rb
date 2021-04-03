221c946003266018825a02106be109f45f829ddb
Mitch Standley <mitch_standley@yahoo.com>
2021-04-03 10:03:35 -0500

    Add new test and code to board

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -23,7 +23,7 @@ describe Board do
     expect(board.cells.keys).to eq(new_cells)
   end
 
-  xit 'has valid coordinates' do
+  it 'has valid coordinates' do
     board = Board.new
 
     expect(board.valid_coordinate?("A1")).to eq(true)
@@ -31,6 +31,15 @@ describe Board do
     expect(board.valid_coordinate?("A5")).to eq(false)
     expect(board.valid_coordinate?("E1")).to eq(false)
     expect(board.valid_coordinate?("A22")).to eq(false)
+  end
+
+  it 'ship has same length as coordinates' do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+
+    expect(board.correct_length?(cruiser, ["A1", "A2"])).to eq(false)
+    expect(board.correct_length?(submarine, ["A2", "A3", "A4"])).to eq (false)
   end
 end
 
