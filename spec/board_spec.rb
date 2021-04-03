b77f3499c43a9cb5c512f3cb2378c511722887d1
Mitch Standley <mitch_standley@yahoo.com>
2021-04-03 11:33:31 -0500

    Renamed test and method on board

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -38,11 +38,7 @@ describe Board do
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)
 
-    expect(board.correct_length?(cruiser, ["A1", "A2"])).to eq(false)
-    expect(board.correct_length?(submarine, ["A2", "A3", "A4"])).to eq (false)
+    expect(board.valid_placement?(cruiser, ["A1", "A2"])).to eq(false)
+    expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to eq (false)
   end
 end
-
-
-
- 
