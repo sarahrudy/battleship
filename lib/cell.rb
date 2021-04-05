ccc8e4fa4aba2b84d7e30cdff456378e270132b9
Mitch Standley <mitch_standley@yahoo.com>
2021-04-05 14:33:46 -0500

    Add annotations to cell.rb

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,38 +1,38 @@
-class Cell
-  attr_reader :coordinate,
-              :ship,
-              :fired_upon
+class Cell #created our cell class.
+  attr_reader :coordinate, #coordinate method.
+              :ship,  #ship method.
+              :fired_upon #fired_upon method
 
-  def initialize(coordinate)
-    @coordinate     = coordinate
-    @ship           = ship
-    @fired_upon     = false
+  def initialize(coordinate) #initialize will take one argument.
+    @coordinate     = coordinate#@coordinate instance variable.
+    @ship           = ship #@ship instance variable
+    @fired_upon     = false #fired_upon intance variable set to false.
   end
 
   def empty? #created empty method that will return true if there is no ship.
-    @ship == nil
+    @ship == nil #@ship doesn't exist
   end
 
-  def place_ship(ship) #places ship passed to argument
-    @ship = ship
+  def place_ship(ship) #places ship passed to argument.
+    @ship = ship #@ship exists.
   end
 
-  def fired_upon? #sets method that returns false unless hit
-    @fired_upon
+  def fired_upon? #sets method that returns false unless hit.
+    @fired_upon #returns false by default.
   end
 
-  def fire_upon #sets method return true if hit
-    if @ship != nil
-      @ship.hit
+  def fire_upon #sets method return true if hit.
+    if @ship != nil #@ship does not equal nil.
+      @ship.hit #@ship.hit returns true, reducing health by 1.
     end
-      @fired_upon = true
+      @fired_upon = true #changes to @fired_upon to return true.
     end
 
-  def render(reveal = false) #renders a cell with it's proper status
-    return "M" if @fired_upon == true && empty?
-    return "X" if @fired_upon == true && @ship.sunk?
-    return "S" if reveal == true && !empty?
-    return "H" if @fired_upon == true && !empty?
-    return "."
+  def render(reveal = false) #renders a cell with it's proper status.
+    return "M" if @fired_upon == true && empty? #cell will render "M" if @fired_upon = true, and cell is empty.
+    return "X" if @fired_upon == true && @ship.sunk? #cell will render "X" if @fired_upon = true, and sunk? method returns true, and prior render does not pass.
+    return "S" if reveal == true && !empty? #cell will render "S" if (reveal = true) and cell isn't empty, and the previous two  renders do not pass.
+    return "H" if @fired_upon == true && !empty? #cell will render "H" if @fired_upon = true and cell isn't empty, as well as having the prior 3 renders not to pass.
+    return "." #in all other cases, cell will render "."
   end
 end
